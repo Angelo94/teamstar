1db2105de70b648cc81b588680fd76aac9960d7e
tommasobellini <dev@tommasobellini.it>
2021-01-16 16:51:54 +0100

    fixed ios path_provider issue

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,13 +1,12 @@
 // ignore: non_constant_identifier_names
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-String base_url = DotEnv().env['BASE_URL'] + '/api/'; // NOTE: or add your address ip
+String base_url = 'http://192.168.1.6:8000' + '/api/'; // NOTE: or add your address ip
 // ignore: non_constant_identifier_names
 String onesignal_key = 'XXXXXXXX-XXXXXXXXXXXX-XXXXXXXXX';
 
-
 // URLS
 String login_api = base_url + 'auth/token/';
-String register_api = base_url + 'auth/register/';
+String register_api = base_url + 'auth/user/';
 String user_api = base_url + 'auth/user/';
 String team_api = base_url + 'team/team/';
